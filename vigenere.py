da86702ce224c5ba8a70e0d1bbeda138a140fe2d
syeomans <samuelyeomans@gmail.com>
2019-10-17 15:06:38 -0400

    Redid vigenere to be a single function and added a substitution cipher

--- vigenere.py
+++ vigenere.py
@@ -1,67 +1,90 @@
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alphabet = list("abcdefghijklmnopqrstuvwxyz")
 
-def encode(plaintext, key):
-	# Make message and key uppercase
-	plaintext = plaintext.upper()
-	key = key.upper()
+def vigenere(inputString, mode, key, preserveCaps=True, preservePunctuation=True):
+	"""
+	The Vigenere cipher is a polyalphabetic substitution cipher that shifts
+	the alphabet by some amount determined by a key per each  plaintext
+	character.
 
-	# Repeat key until its length matches the length of the message. Preserve whitespace and punctuation.
-	keystream = ""
-	i = 0
-	for letter in plaintext:
-		# Preserve whitespace and punctuation
-		if letter  not in alphabet:
-			keystream += letter
+	The vigenere cipher first expands the key into a keystream to match the
+	length of the plaintext. For each pair of plaintext and keystream characters
+	(when encoding), the cipher shifts the substitution alphabet by the position
+	of the keystream character in the  original alphabet and applies the shifted
+	alphabet to the plaintext character.
+
+	Example:
+		Plaintext:	ATTACKATDAWN
+		Key:		AZTEC
+		Keystream:	AZTECAZTECAZ
+		Ciphertext:	ASMEEKZMHCWM
+
+	In the above example, the key AZTEC was expanded to be 12 characters long.
+	For the first character encoding, the alphabet was shifted by A, which
+	evaluates to 0 (or 26 depending on how you look at it) shifts to the right.
+	The second character Z shifts by 25 places, the third character T shifts by
+	20 places, and so on.
+
+	Key character 	number of shifts
+	A 				0
+	B 				1
+	C 				2
+	...
+	Z 				25
+
+	Inputs:
+		inputString: plaintext if encrypting or ciphertext if decrypting
+		mode: 'e' for encryption or 'd' for decryption
+		key: a string used to encrypt the message
+		preserveCaps: boolean to preserve capitalization (default: True)
+		preservePunctuation: boolean to preserve punctuation including spaces
+		(default: True)
+
+	Outputs:
+		Returns a plaintext string if [mode] is 'd' or a ciphertext string if
+		[mode] is 'e'
+	"""
+	# Sanitize inputs
+	key = key.lower()
+	key.replace(" ", "")
+	if mode not in ['e', 'd']:
+		raise Exception("'mode' must be either 'e' for encryption or 'd' for decryption")
+
+	# Initialize variables
+	keystreamCounter = -1
+	outString = ""
+
+	# Read input string. Iterate on each character. Increment counter at each loop.
+	for inputChar in inputString:
+		if inputChar.lower() in alphabet:
+			keystreamCounter += 1
+			# Determine the number of positions to move the plaintext character given the key
+			numMoves = alphabet.index(key[keystreamCounter % len(key)])
+			# Move the plaintext character
+			if mode == 'e':
+				outputChar = alphabet[(alphabet.index(inputChar.lower()) + numMoves) % len(alphabet)]
+			elif mode == 'd':
+				outputChar = alphabet[(alphabet.index(inputChar.lower()) - numMoves) % len(alphabet)]
+
+			# Preserve caps if specified
+			if inputChar.isupper() & preserveCaps:
+				outputChar = outputChar.upper()
+
+		# Preserve punctutation if specified
+		elif preservePunctuation:
+			outputChar = inputChar
+
+		# Otherwise, do nothing
 		else:
-			keystream += key[i % len(key)]
-			# Only increment counter in this case
-			i += 1
+			outputChar = ""
 
-	# Encode message 
-	ciphertext = ""
-	i = 0
-	for letter in plaintext:
-		# Preserve whitespace and punctuation
-		if letter  not in alphabet:
-			ciphertext += letter
-		# Offset this letter based on the keystream
-		else:
-			alphabetPosition = alphabet.index(letter)
-			offset = alphabet.index(keystream[i])
-			ciphertext += alphabet[alphabetPosition + offset]
-		# In either case, increment counter
-		i += 1
-	return(ciphertext)
+		# Append to output string
+		outString += outputChar
 
-def decode(ciphertext, key):
-	# Make key uppercase
-	key = key.upper()
+	# Write to file
+	return(outString)
 
-	# Repeat key until its length matches the length of the message. Preserve whitespace and punctuation.
-	keystream = ""
-	i = 0
-	for letter in ciphertext:
-		# Preserve whitespace and punctuation
-		if letter  not in alphabet:
-			keystream += letter
-		else:
-			keystream += key[i % len(key)]
-			# Only increment counter in this case
-			i += 1
-
-	# Decode message 
-	plaintext = ""
-	i = 0
-	for letter in ciphertext:
-		# Preserve whitespace and punctuation
-		if letter  not in alphabet:
-			plaintext += letter
-		# Offset this letter based on the keystream
-		else:
-			alphabetPosition = alphabet.index(letter)
-			offset = alphabet.index(keystream[i])
-			plaintext += alphabet[alphabetPosition - offset]
-		# In either case, increment counter
-		i += 1
-	return(plaintext)
-
+# Test script
+# inputString = open('plaintext.txt', 'r').read()
+inputString = "ATTACKATDAWN"
+outputString = vigenere(inputString, 'e', 'AZTEC')
+print(outputString)
